ef86251a3899764e1df2f74a12aec9fd58a8b8ea
GODOL <sihyun9346@mokwon.ac.kr>
2025-03-26 17:30:37 +0900

    복구

--- RollABall/Assets/Scripts/CameraController.cs
+++ RollABall/Assets/Scripts/CameraController.cs
@@ -1,17 +1,21 @@
 using UnityEngine;
 public class CameraController : MonoBehaviour
 {
-    [SerializeField]
-    GameObject player;
-    Vector3 offset;
-    void Start()
+    private GameObject player;
+    private Vector3 offset;
+    public void SetupPlayer(GameObject _player)
     {
-        // player > camera Vector
-        offset = this.transform.position - player.transform.position;
+        // 플레이어에서 카메라로 가는 벡터
+        player = _player;
+        // offset = this.transform.position - player.transform.position;
+
+        offset = -player.transform.forward * 5f + player.transform.up * 2f;
     }
 
-    void LateUpdate()
+    private void LateUpdate()
     {
+        if (player is null) return;
+
         this.transform.position = player.transform.position + offset + Vector3.up * 2;
         this.transform.LookAt(player.transform);
     }
--- RollABall/Assets/Scripts/PlayerController.cs
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -1,34 +1,86 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class PlayerController : MonoBehaviour
+using Mirror;
+public class PlayerController : NetworkBehaviour
 {
     PlayerInputAction playerInput;
 
+    public TextMesh playerNameText;
+    public GameObject flaotingInfo;
+
+    [SyncVar(hook = nameof(OnNameChanged))]
+    public string playerName;
+
+
+    [SyncVar(hook = nameof(OnColorChanged))]
+    public Color playerColor = Color.white;
+
+    Material playerMaterialClone;
+
     [SerializeField]
-    private Rigidbody rb;
+    Rigidbody rb;
 
-    private float movementX;
-    private float movementY;
+    float movementX;
+    float movementY;
 
-    const float MOVE_FORCE = 100f;
+    const float MOVE_FORCE = 1000f;
 
-    void Start()
+    void OnNameChanged(string _old, string _new)
     {
-        playerInput = new PlayerInputAction();
-        playerInput.Player.Enable();
-        playerInput.Player.Move.performed += ctx => this.OnMove(ctx);
-        playerInput.Player.Move.canceled += ctx => this.OnMove(ctx);
+        playerNameText.text = playerName;
     }
 
+    void OnColorChanged(Color oldColor, Color newColor)
+    {
+        Renderer currRender = this.GetComponent<Renderer>();
+        playerMaterialClone = new Material(currRender.material);
+        playerMaterialClone.color = newColor;
+        currRender.material = playerMaterialClone;
+
+        string name = $"Player {Random.Range(100, 1000)}";
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        Debug.Log("S S S Start Local Player");
+        Camera mainCam = Camera.main;
+        var camController = mainCam.GetComponent<CameraController>();
+        camController.SetupPlayer(this.gameObject);
+
+        playerInput = new PlayerInputAction();
+        playerInput.Player.Enable();
+        playerInput.Player.Move.performed += ctx => this.OnMovement(ctx);
+        playerInput.Player.Move.canceled += ctx => this.OnMovement(ctx);
+
+        Color color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        CmdSetupPlayer(name, color);
+
+        flaotingInfo.transform.parent = null;
+    }
+
+    [Command]
+    public void CmdSetupPlayer(string name, Color color)
+    {
+        playerName = name;
+        playerColor = color;
+    }
 
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0f, movementY) * MOVE_FORCE * Time.deltaTime;
         rb.AddForce(movement);
+
+        Vector3 pos = this.transform.position;
+        flaotingInfo.transform.position = pos + Vector3.up * 1.5f;
     }
 
-    public void OnMove(InputAction.CallbackContext ctx)
+    public void OnMovement(InputAction.CallbackContext ctx)
     {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
         if (ctx.performed)
         {
             Vector2 movementVector = ctx.ReadValue<Vector2>();
