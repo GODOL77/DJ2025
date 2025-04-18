9bc5a9f7ed6dde07469d6d23c2a981f059621638
GODOL77 <89023025+GODOL77@users.noreply.github.com>
2025-04-18 17:53:58 +0900

    JungSangHwa

--- Zombie/Assets/Scripts/Gun.cs
+++ Zombie/Assets/Scripts/Gun.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
+using System.Runtime.Serialization;
 using UnityEngine;
 
 // 총을 구현한다
-public class Gun : MonoBehaviour {
+public class Gun : MonoBehaviour
+{
     // 총의 상태를 표현하는데 사용할 타입을 선언한다
-    public enum State {
+    public enum State
+    {
         Ready, // 발사 준비됨
         Empty, // 탄창이 빔
         Reloading // 재장전 중
@@ -17,14 +20,14 @@ public class Gun : MonoBehaviour {
     public ParticleSystem muzzleFlashEffect; // 총구 화염 효과
     public ParticleSystem shellEjectEffect; // 탄피 배출 효과
 
-    private LineRenderer bulletLineRenderer; // 총알 궤적을 그리기 위한 렌더러
+    LineRenderer bulletLineRenderer; // 총알 궤적을 그리기 위한 렌더러
 
-    private AudioSource gunAudioPlayer; // 총 소리 재생기
+    AudioSource gunAudioPlayer; // 총 소리 재생기
     public AudioClip shotClip; // 발사 소리
     public AudioClip reloadClip; // 재장전 소리
 
     public float damage = 25; // 공격력
-    private float fireDistance = 50f; // 사정거리
+    float fireDistance = 50f; // 사정거리
 
     public int ammoRemain = 100; // 남은 전체 탄약
     public int magCapacity = 25; // 탄창 용량
@@ -33,15 +36,26 @@ public class Gun : MonoBehaviour {
 
     public float timeBetFire = 0.12f; // 총알 발사 간격
     public float reloadTime = 1.8f; // 재장전 소요 시간
-    private float lastFireTime; // 총을 마지막으로 발사한 시점
+    float lastFireTime; // 총을 마지막으로 발사한 시점
 
 
-    private void Awake() {
+    void Awake()
+    {
         // 사용할 컴포넌트들의 참조를 가져오기
+        gunAudioPlayer = GetComponent<AudioSource>();
+        bulletLineRenderer = GetComponent<LineRenderer>();
+
+        //  사용할 점 위치 변경 : 2개
+        bulletLineRenderer.positionCount = 2;
+        bulletLineRenderer.enabled = false;
     }
 
-    private void OnEnable() {
+    void OnEnable()
+    {
         // 총 상태 초기화
+        magAmmo = magCapacity;
+        state = State.Ready;
+        lastFireTime = 0;
     }
 
     // 발사 시도
@@ -49,31 +63,34 @@ public class Gun : MonoBehaviour {
     {
         if (state == State.Ready && Time.time >= lastFireTime + timeBetFire)
         {
-            lastFireTime = time.time;
+            lastFireTime = Time.time;
             Shot();
         }
     }
 
     // 실제 발사 처리
-    private void Shot()
+    void Shot()
     {
         RaycastHit hit;
 
-        Vector3 hitPosition = Vector3.zer;
+        Vector3 hitPosition = Vector3.zero;
 
         if (Physics.Raycast(fireTransform.position, fireTransform.forward, out hit, fireDistance))
         {
             IDamageable target = hit.collider.GetComponent<IDamageable>();
-        }
-        //target?.OnDamage(damage, hit.point, hit.normal);
+            if (target != null)
+            {
+                target.OnDamage(damage, hit.point, hit.normal);
+            }
+            // target?.OnDamage(damage, hit.point, hit.normal);
             hitPosition = hit.point;
+        }
         else
         {
-            hitPosition = fireTransform.position + fireTransform.forward * fireDistance;
+            hitPosition = fireTransform.position + transform.forward * fireDistance;
         }
 
         StartCoroutine(ShotEffect(hitPosition));
-
         magAmmo--;
         if (magAmmo <= 0)
         {
@@ -82,16 +99,18 @@ public class Gun : MonoBehaviour {
     }
 
     // 발사 이펙트와 소리를 재생하고 총알 궤적을 그린다
-    private IEnumerator ShotEffect(Vector3 hitPosition) {
-
+    IEnumerator ShotEffect(Vector3 hitPosition)
+    {
         muzzleFlashEffect.Play();
+
         shellEjectEffect.Play();
-        gunAudioPlayer.PlayOnShot(shotClip);
+
+        gunAudioPlayer.PlayOneShot(shotClip);
+
+        yield return new WaitForSeconds(shotClip.length);
+
         bulletLineRenderer.SetPosition(0, fireTransform.position);
         bulletLineRenderer.SetPosition(1, hitPosition);
-
-        // yield return new WaitforSeconds(shotClip.length);
-        // video.loopPointReached += (v) => v.Pause();
 
         // 라인 렌더러를 활성화하여 총알 궤적을 그린다
         bulletLineRenderer.enabled = true;
@@ -106,21 +125,26 @@ public class Gun : MonoBehaviour {
     // 재장전 시도
     public bool Reload()
     {
-        if (state == State.Relead || ammoRemain <= 0 | magAmmo >= magCapacity)
+        if (state == State.Reloading || ammoRemain <= 0 || magAmmo >= magCapacity)
         {
             return false;
         }
-        
+
         // 재장전
         StartCoroutine(ReloadRoutine());
+
         return false;
     }
 
     // 실제 재장전 처리를 진행
-    private IEnumerator ReloadRoutine()
+    IEnumerator ReloadRoutine()
     {
+        // 현재 상태를 재장전 중 상태로 전환
         state = State.Reloading;
-        gunAudioPlayer.PlayOnShot(reloadClip);
+
+        gunAudioPlayer.PlayOneShot(reloadClip);
+
+        // 재장전 소요 시간 만큼 처리를 쉬기
         yield return new WaitForSeconds(reloadTime);
 
         int ammoToFill = magCapacity - magAmmo;
@@ -131,14 +155,7 @@ public class Gun : MonoBehaviour {
         }
 
         magAmmo += ammoToFill;
-
         ammoRemain -= ammoToFill;
-
-        // 현재 상태를 재장전 중 상태로 전환
-        // state = State.Reloading;
-        
-        // 재장전 소요 시간 만큼 처리를 쉬기
-        // yield return new WaitForSeconds(reloadTime);
 
         // 총의 현재 상태를 발사 준비된 상태로 변경
         state = State.Ready;
--- Zombie/Assets/Scripts/PlayerMovement.cs
+++ Zombie/Assets/Scripts/PlayerMovement.cs
@@ -1,34 +1,29 @@
 ﻿using UnityEngine;
 
 // 플레이어 캐릭터를 사용자 입력에 따라 움직이는 스크립트
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
     public float moveSpeed = 5f; // 앞뒤 움직임의 속도
     public float rotateSpeed = 180f; // 좌우 회전 속도
 
-
-    [SerializeField] private PlayerInput playerInput; // 플레이어 입력을 알려주는 컴포넌트
-    [SerializeField] private Rigidbody playerRigidbody; // 플레이어 캐릭터의 리지드바디
-    [SerializeField] private Animator playerAnimator; // 플레이어 캐릭터의 애니메이터
-
-    private void Start()
-    {
-        // 사용할 컴포넌트들의 참조를 가져오기
-    }
+    [SerializeField] PlayerInput playerInput; // 플레이어 입력을 알려주는 컴포넌트
+    [SerializeField] Rigidbody playerRigidbody; // 플레이어 캐릭터의 리지드바디
+    [SerializeField] Animator playerAnimator; // 플레이어 캐릭터의 애니메이터
 
     // FixedUpdate는 물리 갱신 주기에 맞춰 실행됨
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         // 물리 갱신 주기마다 움직임, 회전, 애니메이션 처리 실행
-
-        Rotate();
         Move();
+        Rotate();
 
-        // 입력값에 따라 애니메이터의 Move 파라미터 값 변경
+        // 입력 값에 따라 애니메이터의 Move 파라미터 값 변경
         playerAnimator.SetFloat("Move", playerInput.move);
+
     }
 
     // 입력값에 따라 캐릭터를 앞뒤로 움직임
-    private void Move()
+    void Move()
     {
         Vector3 moveDistance = playerInput.move * transform.forward * moveSpeed * Time.deltaTime;
 
@@ -36,7 +31,7 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     // 입력값에 따라 캐릭터를 좌우로 회전
-    private void Rotate()
+    void Rotate()
     {
         float turn = playerInput.rotate * rotateSpeed * Time.deltaTime;
 
